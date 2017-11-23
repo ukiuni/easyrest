f672496eafb0410375883743cd7313353b2658b3
ukiuni <ukiuni@gmail.com>
2017-11-23 12:51:52 +0900

    update shell

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 :loop
   set JAVA_HOME="C:\Program Files\Java\jdk1.8.0_152"
-  ./gradrew.bat --no-daemon clean && ./gradrew.bat --no-daemon test
+  .\gradrew.bat --no-daemon clean && .\gradrew.bat --no-daemon test
   set JAVA_HOME="C:\Program Files\Zulu\zulu-8"
-  ./gradrew.bat --no-daemon clean && ./gradrew.bat --no-daemon test
+  .\gradrew.bat --no-daemon clean && .\gradrew.bat --no-daemon test
 goto loop
