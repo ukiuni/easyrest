581002f3f5deb6c6dbe537ad4f8c708d0f8575db
ukiuni <ukiuni@gmail.com>
2017-11-23 12:50:29 +0900

    update shell

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 function run () {
   export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_152.jdk/Contents/Home/
-  gradle --no-daemon clean && gradle --no-daemon test
+  ./gradlew --no-daemon clean && ./gradlew --no-daemon test
   export JAVA_HOME=/Applications/zulu8.25.0.1-jdk8.0.152-macosx_x64
-  gradle --no-daemon clean && gradle --no-daemon test
+  ./gradlew --no-daemon clean && ./gradlew --no-daemon test
 #	export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home/
 #	gradle --no-daemon clean && gradle --no-daemon test
 #	export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/	
